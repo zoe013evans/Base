4cd2be15e3d9818a2f9e46323ffce64055bc955e
Zoe <ezoe013@gmail.com>
2024-07-11 13:51:25 +0100

    stickler patrolling code

--- tasks/stickler/scripts/main.py
+++ tasks/stickler/scripts/main.py
@@ -10,10 +10,33 @@ from shapely.geometry import Polygon as ShapelyPolygon
 from std_msgs.msg import Header
 
 if __name__ == "__main__":
-    rospy.init_note("stickler_robocup")
-    stickler = SticklerForTheRules()
+    rospy.init_node("stickler_robocup")
+
+    living_room_area = rospy.get_param("stickler/living_room")
+
+    living_room_pose_param = rospy.get_param("stickler/living_room_pose")
+    living_room_pose = Pose(
+        position=Point(**living_room_pose_param["position"]),
+        orientation = Quaternion(**living_room_pose_param["orientation"])
+    )
+
+    forbidden_room_area = rospy.get_param("stickler/forbidden_room")
+    forbidden_room_pose_param = rospy.get_param("stickler/forbidden_room_pose")
+
+    forbidden_room_pose = Pose(
+        position=Point(**forbidden_room_pose_param["position"]),
+        orientation = Quaternion(**forbidden_room_pose_param["orientation"])
+    )
+
+
+    entrance_room_area = rospy.get_param("stickler/entrance")
+
+    stickler = SticklerForTheRules(
+        living_room_pose,
+        living_room_area,
+        forbidden_room_pose,
+        forbidden_room_area
+    )
     outcome = stickler.execute()
     rospy.loginfo(f"Stickler for the Rules finished with outcome: {outcome}")
     rospy.spin()
-
-    
--- tasks/stickler/src/stickler/state_machine.py
+++ tasks/stickler/src/stickler/state_machine.py
@@ -22,27 +22,72 @@ from lasr_skills import (
 class SticklerForTheRules(smach.StateMachine):
     def __init__(
             self,
+            living_room_pose: Pose,
+            living_room_area: Polygon,
+            forbidden_room_pose: Pose,
+            forbidden_room_area: Polygon,
+
+
     ):
         smach.StateMachine.__init__(self,outcomes=["succeeded","failed"])
+        self.living_room_pose = living_room_pose
+        self.living_room_area = living_room_area
+        self.forbidden_room_pose = forbidden_room_pose
+        self.forbidden_room_area = forbidden_room_area
 
-        smach.StateMachine.add(
-            "SAY_START",
-            Say(text="Start of Stickler For the Rules. Don't break the rules!"),
-            transitions={
-                    "succeeded": "SAY_END",
-                    "aborted": "SAY_END",
-                    "preempted": "SAY_END",
-                },
 
-        )
+        # Loop: 
 
-        smach.StateMachine.add(
-            "SAY_END",
-            Say(text="I am done!"),
-            transitions={
-                    "succeeded": "succeeded",
-                    "aborted": "failed",
-                    "preempted": "succeeded",
-                },
+        with self:
 
-        )
+            smach.StateMachine.add(
+                "SAY_START",
+                Say(text="Start of Stickler For the Rules. Don't break the rules!"),
+                transitions={
+                        "succeeded": "GO_TO_FORBIDDEN_ROOM",
+                        "aborted": "SAY_END",
+                        "preempted": "SAY_END",
+                    },
+
+            )
+
+            smach.StateMachine.add(
+                "GO_TO_FORBIDDEN_ROOM",
+                GoToLocation(self.forbidden_room_pose),
+                transitions={"succeeded": "ANNOUNCE_FORBIDDEN_ROOM",
+                            "failed": "failed"}
+        
+            )
+            smach.StateMachine.add(
+                "ANNOUNCE_FORBIDDEN_ROOM",
+                Say(text="I'm checking the forbidden room"),
+                transitions={"succeeded": "GO_TO_LIVING_ROOM",
+                            "aborted": "failed",
+                            "preempted": "failed",}
+            )
+            smach.StateMachine.add(
+                "GO_TO_LIVING_ROOM",
+                GoToLocation(self.living_room_pose),
+                transitions={"succeeded": "ANNOUNCE_LIVING_ROOM",
+                            "failed": "failed"}
+                
+            )
+
+            smach.StateMachine.add(
+                "ANNOUNCE_LIVING_ROOM",
+                Say(text="I'm checking the living room"),
+                transitions={"succeeded": "GO_TO_FORBIDDEN_ROOM",
+                            "aborted": "failed",
+                            "preempted": "failed"}
+
+            )
+
+            smach.StateMachine.add(
+                "SAY_END",
+                Say(text="I am done!"),
+                transitions={
+                        "succeeded": "succeeded",
+                        "aborted": "failed",
+                        "preempted": "succeeded",
+                    },
+            )
